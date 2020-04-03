1ac55b21556bf3ef9f812bc79e591b16c9ffc8ac
radillaowena <radilla_nathaniel_27rpl@student.smktelkom-mlg.sch.id>
2020-04-03 22:13:50 +0700

    ubah

--- src/main.js
+++ src/main.js
@@ -54,15 +54,3 @@ new Vue({
 
 
 
-// import Vue from 'vue'
-// import App from './App.vue'
-// import router from './router'
-// import store from './store'
-
-// Vue.config.productionTip = false
-
-// new Vue({
-//   router,
-//   store,
-//   render: h => h(App)
-// }).$mount('#app')
